11a8c55c2dc7604ea8f492f2a64d7bf78f293edd
jasonbgilliam-source <jason.b.gilliam@gmail.com>
2025-09-15 10:01:13 +0100

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -13,3 +13,10 @@ export function supabaseServer() {
     },
   });
 }
+// lib/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
