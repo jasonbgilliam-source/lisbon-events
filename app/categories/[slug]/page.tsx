3a1e8f4c971d4757ad7ea59be05892fed93be19a
jasonbgilliam-source <jason.b.gilliam@gmail.com>
2025-10-03 15:18:56 +0100

    Update page.tsx

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,27 +1,10 @@
-// app/categories/[slug]/page.tsx
-import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
-import ConsentGate from "@/app/components/ConsentGate";
+import React from "react";
+import { useParams } from "next/navigation";
+import ConsentGate from "../../components/ConsentGate";
 
-interface CategoryPageProps {
-  params: { slug: string };
-}
-
-export default function CategoryPage({ params }: CategoryPageProps) {
-  const { slug } = params;
-  const cookieStore = cookies();
-  const consent = cookieStore.get("cookie_consent");
-
-  if (!slug) {
-    notFound();
-  }
+export default function CategoryPage() {
+  const params = useParams();
+  const slug = params?.slug;
 
   return (
-    <ConsentGate consent={consent?.value}>
-      <main className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Category: {slug}</h1>
-        <p>Here we will render events for this category.</p>
-      </main>
-    </ConsentGate>
-  );
-}
+    <Consent
