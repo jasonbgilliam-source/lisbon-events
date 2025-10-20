e44bbc2c960f9db53396c42fca3f18c89d70a2b6
jasonbgilliam-source <jason.b.gilliam@gmail.com>
2025-10-20 14:50:47 +0100

    Update page.tsx

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -87,9 +87,9 @@ export default function CategoryPage({
           </p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {events.map((event) => (
-              <EventCard key={event.id} event={event} />
-            ))}
+             {events.map((event) => (
+          <EventCard key={event.id} e={event} />
+          ))}
           </div>
         )}
       </section>
